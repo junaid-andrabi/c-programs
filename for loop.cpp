7105c18b1c5bd10f2f272f1471e8962235a9979a
_junaid_andrabi <107830004+junaid-andrabi@users.noreply.github.com>
2022-09-21 13:24:43 -0400

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,14 @@
+//loop for loop (500  498 496 ... 2)
+#include<stdio.h>
+ int main()
+   
+   
+   
+   {
+   	
+   	 int i;
+   	 for( i=500;i>1;i=i-2)
+   	 { printf("%d ",i );
+   	 }
+     return 0;
+   }
