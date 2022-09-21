c6b8e4793a147807a51ece5d8662dc2fdff069e8
_junaid_andrabi <107830004+junaid-andrabi@users.noreply.github.com>
2022-09-21 13:33:53 -0400

    Add files via upload

---
+++ series.c
@@ -0,0 +1,18 @@
+//program in c to display square  series
+#include<stdio.h>
+ int main ()
+  {
+  	
+  	int i,n;
+  	 printf("enter the value for n upto you want to find square series \n");
+  	 scanf("%d",&n);
+  	    printf("the square is\n");
+  	      for(i=2;i<=n;i=i*2)
+  	       
+  	       printf("%d \n",i); //here we can also use,\t for tab,\\ for backslash,...
+  	       
+  	       
+ 
+   return 0;
+   
+  }
