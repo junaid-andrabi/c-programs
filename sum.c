5d55c778ea49e84e18ca6264dc57793267961ed4
_junaid_andrabi <107830004+junaid-andrabi@users.noreply.github.com>
2022-09-21 12:32:20 -0400

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+//Program in c to add two numbers.
+#include<stdio.h>
+  int main()
+   {
+   	int a,b,c;
+   	printf("Enter two numbers you want to add\n");
+  	 scanf("%d %d",&a,&b);
+  	 c=a+b;
+  	 printf("The sum of two entered numbers is: %d",c);
+    return 0;
+   }
