cbfd063e07a07c306828ec61e21ef3b6559b05a6
_junaid_andrabi <107830004+junaid-andrabi@users.noreply.github.com>
2022-09-21 13:25:57 -0400

    Add files via upload

---
+++ for loop2.c
@@ -0,0 +1,15 @@
+// 2nd  programe for loop (ten times my name)
+ 
+#include<stdio.h>
+ int main ()
+  {
+  	int i;
+  	
+  	
+  	      for(i=1;i<=10;i++)
+  	     {
+  	      printf("syed juanid \n");
+  	     }
+  	     return 0;
+  	     
+  }
